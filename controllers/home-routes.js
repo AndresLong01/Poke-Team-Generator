54f09650b3ef7eb8d3390675ea7220129523a945
Andres Long <andreslong95@gmail.com>
2022-12-14 01:38:53 -0600

    fixed route into splash page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('all-posts');
+    res.render('splash-page');
   } catch (err) {
     res.status(500).json(err);
   }
